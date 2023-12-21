42668e545d8c0df2a659294d619d749c7f407bab
lamphongthanh1992 <lamphongthanh1992@gmail.com>
2023-12-21 16:04:43 +0700

    SpringBoot
    create project with spring boot

---
+++ src/main/java/com/basic/BasicSellApp.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSellApp {
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSellApp.class, args);
+	}
+}
---
+++ src/main/java/com/basic/controller/BasicController.java
@@ -0,0 +1,16 @@
+package com.basic.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class BasicController {
+	@GetMapping
+    public String list() {
+        return "test";
+    }
+}
